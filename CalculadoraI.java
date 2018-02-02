5c2a87baa63580290d1ca3729d3ea86aa775c3c6
estebancabreraa <esteban.cabrera.a@gmail.com>
2018-02-01 21:41:59 -0600

    Clase CALCULADORA terminada.
    Clase CALCULADORA terminada.

---
+++ CalculadoraI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Oliver Mazariegos 16043
+ */
+public interface CalculadoraI {
+    /**
+    * @param string de lo que se desea calcular
+    * @return string con la respuesta
+    */
+    public String calcular(String string);
+    
+}
---
+++ StackVector.java
@@ -0,0 +1,48 @@
+package calculadora;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Revilo
+ * @param <E> generic
+ */
+public class StackVector<E> implements Stack<E> {
+ protected ArrayList<E> data;
+    
+    public StackVector(){
+        data = new ArrayList<E>();
+    }
+    
+    @Override
+    public void push(E item) {
+        data.add(item);
+    }
+
+    @Override
+    public E pop() {
+        return data.remove(size()-1);
+    }
+
+    @Override
+    public E peek() {
+        return data.get(size()-1);
+    }
+
+    @Override
+    public boolean empty() {
+        return size()== 0;
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+    
+}
