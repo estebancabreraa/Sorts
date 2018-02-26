8f51bcb7f56bb3e86992acf428f6410f0e08c0f2
Oliversinn <32202318+Oliversinn@users.noreply.github.com>
2018-02-26 11:23:50 -0600

    Update RadixSort.java

--- Sorts/src/Sorts/RadixSort.java
+++ Sorts/src/Sorts/RadixSort.java
@@ -5,11 +5,6 @@
  */
 package Sorts;
 
-/**
- *
- * @author Revilo
- */
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
@@ -17,11 +12,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class QuickSort {
-	
-	public static void main(String[] args){
-            QuickSort app = new QuickSort();
-            
+/**
+ *
+ * @author Revilo
+ */
+public class RadixSort {
+    
+    public static void main(String[] args){
+            RadixSort app = new RadixSort();
             Scanner reader = new Scanner(System.in);  // Reading from System.in
             
             System.out.println("Enter a number: ");
@@ -29,7 +27,7 @@ public class QuickSort {
             
             int[] input = app.generateRandomNumbers(n);
             
-            System.out.println("QUICK SORT");
+            System.out.println("RADIX SORT");
             System.out.println("");
             
             System.out.println("Sort Inicial");
@@ -37,46 +35,67 @@ public class QuickSort {
             
             System.out.println(" ");
             System.out.println("Sort final");
-            System.out.println(Arrays.toString(app.QuickSort(input)));
+            System.out.println(Arrays.toString(app.RadixSort(input)));
             
         }
-	
-	
-	public int [] QuickSort(int numeros[]){
-		return quicksort2(numeros,0,numeros.length-1);
-	}
-	
-	public int[] quicksort2(int numeros[],int izq, int der){
-		if(izq>=der)
-			return numeros;
-		int i=izq,d=der;
-		if(izq!=der){
-			int pivote;
-			int aux;
-			pivote=izq;
-			while (izq!=der){
-				while (numeros[der]>=numeros[pivote] && izq<der)
-					der--;
-				while(numeros[izq]<numeros[pivote] && izq<der)
-					izq++;
-				if(der!=izq)
-				{
-					aux = numeros[der];
-					numeros[der]=numeros[izq];
-					numeros[izq]=aux;
-				}
-				if(izq==der){
-					quicksort2(numeros,i,izq-1);
-					quicksort2(numeros,izq+1,d);
-				}
-			}	
-					
-		}
-		else
-			return numeros;
-		return numeros;
-	}
-         private int[] generateRandomNumbers(int n){
+    
+     // The main function to that sorts arr[] of size n using
+    // Radix Sort
+    private int[] RadixSort(int arr[])
+    {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, 3000);
+ 
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m/exp > 0; exp *= 10)
+            countSort(arr, 3000, exp);
+        return arr;
+    }
+    
+    // A utility function to get maximum value in arr[]
+    static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+ 
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count,0);
+ 
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+ 
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+ 
+        // Build the output array
+        for (i = n - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+ 
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+    
+    private int[] generateRandomNumbers(int n){
             StringBuffer cadena = new StringBuffer();
             int[] result = new int[n];
             Random random = new Random();
@@ -95,7 +114,4 @@ public class QuickSort {
         }  
             return result;
         }
-				
 }
-
-
